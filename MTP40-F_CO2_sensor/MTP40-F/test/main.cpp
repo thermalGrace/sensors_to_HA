2649412baf1f6a01fad26836758cafe95ee3f255
BartoszKaszuba <bartoszkasz2005@gmail.com>
2026-01-01 20:44:25 +0100

    refactor: correct the file names

---
+++ MTP40-F_CO2_sensor/MTP40-F/test/main.cpp
@@ -0,0 +1,37 @@
+
+
+#include "MTP40F.h"
+
+const int MTP40F_RX_PIN = 6; // Pico GPIO 6 connects to MTP40F TX (pin 6)
+const int MTP40F_TX_PIN = 7; // Pico GPIO 7 connects to MTP40F RX (pin 7)
+
+MTP40F mtp(&Serial1);
+
+int lines = 10;
+
+void setup()
+{
+  Serial1.begin(9600);
+  mtp.begin(); 
+  
+}
+
+void loop()
+{
+  if (lines == 10)
+  {
+    lines = 0;
+    Serial.println("\nTIME\tCO2 LEVEL");
+  }
+
+  if (millis() - mtp.lastRead() >= 2500)
+  {
+    Serial.print(millis());
+    Serial.print("\t");
+    Serial.print(mtp.getGasConcentration());
+    Serial.println();
+    lines++;
+  }
+}
+
+//  -- END OF FILE --
